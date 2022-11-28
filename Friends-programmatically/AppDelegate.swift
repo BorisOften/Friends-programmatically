b0a252a6f874e3013810f1beafbb85aca2d377be
Boris <banofon@gmail.com>
2022-11-28 16:57:01 -0600

    First build

---
+++ Friends-programmatically/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Friends-programmatically
+//
+//  Created by Boris Ofon on 11/28/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
